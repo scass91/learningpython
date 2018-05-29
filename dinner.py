2f2803100773ef103315b3df47b0d8b29892dabc
scass91 <scass1991@gmail.com>
2018-05-29 13:08:09 +0100

    adding small list related project

---
+++ dinner.py
@@ -0,0 +1,5 @@
+##list of guests
+dinner_guests = ["stir", "paul bohill", "jake peralta"]
+print(dinner_guests[0].title() + ", merrrrrrr, I'm throwing a sweet tea, can you come?")
+print(dinner_guests[1].title() + ", if you can't make this dinner party, i'll take it away!")
+print(dinner_guests[2].title() + ", I've got so many recommendations from Boyle!")
