8a21340d068ea15856e319e6912460a2da588e93
scass91 <scass1991@gmail.com>
2018-05-29 13:15:58 +0100

    changing lists as guest attendance evolves

--- dinner.py
+++ dinner.py
@@ -1,5 +1,14 @@
-##list of guests
+###list of guests, comment out original invites as it turns out one can't attend
 dinner_guests = ["stir", "paul bohill", "jake peralta"]
+###print(dinner_guests[0].title() + ", merrrrrrr, I'm throwing a sweet tea, can you come?")
+###print(dinner_guests[1].title() + ", if you can't make this dinner party, i'll take it away!")
+###print(dinner_guests[2].title() + ", I've got so many recommendations from Boyle!")
+
+###Peralta cannot attend
+print(dinner_guests[2].title() + ", I'm sorry to hear you won't be able to make it!\n")
+
+###replace Peralta
+dinner_guests[2] = "mark noble"
 print(dinner_guests[0].title() + ", merrrrrrr, I'm throwing a sweet tea, can you come?")
 print(dinner_guests[1].title() + ", if you can't make this dinner party, i'll take it away!")
-print(dinner_guests[2].title() + ", I've got so many recommendations from Boyle!")
+print(dinner_guests[2].title() + ", now it's post-season, come for dinner!")
