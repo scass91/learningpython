33fb635ba565ac5fdbbade59eb24c55d6e88c452
scass91 <scass1991@gmail.com>
2018-06-01 23:54:13 +0100

    first if statements

---
+++ cars.py
@@ -0,0 +1,29 @@
+###June 1st 2018 - if statements!
+###if my car is what I am searching for, it should return in all caps
+###= single sign sets the values
+###== double sign is a true/false checker - CaSe SeNsItIvE
+
+cars = ["bmw", "audi", "ford", "vauxhall"]
+for car in cars:
+    if car == "bmw":
+        print(car.upper())
+    else:
+        print(car.title())
+
+###inequality testing != is the not equal operator
+
+car_colour = "grey"
+
+if car_colour != "yellow":
+    print("\nThat's the wrong colour!\n")
+
+
+###checking if a value is in a list
+cars = ["bmw", "audi", "ford", "vauxhall"]
+new_car = "skoda"
+
+if new_car not in cars:
+    print(new_car.title() + " must be a new manufacturer")
+
+
+###booleans are either true or false
