c4696ecfec9407b9b27686b2ae046dddf78bd3ab
scass91 <scass1991@gmail.com>
2018-06-21 12:19:59 +0100

    initial commit

---
+++ alien.py
@@ -0,0 +1,18 @@
+###June 18th 2018
+###Dictionary stores info about an alien below
+###Key-value pairs here i.e. points : 5
+alien_0 = {'colour': 'green', 'points' : 5}
+
+###print(alien_0['colour'])
+###print(alien_0['points'])
+
+###new_points = alien_0['points']
+###print("You just earned " + str(new_points) + " points!")
+
+###Add new key-value pairs
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+
+print(alien_0)
---
+++ favourite_languages.py
@@ -0,0 +1,11 @@
+###June 21st 2018
+favourite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+    }
+
+print("Sarah's favourite language is " +
+    favourite_languages['sarah'].title() +
+    ".")
