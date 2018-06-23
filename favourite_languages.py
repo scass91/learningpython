dc6efee76de10f9a7b9eb1c88f4e61cea52ef1b2
scass91 <scass1991@gmail.com>
2018-06-23 11:34:16 +0100

    looping through keys

--- favourite_languages.py
+++ favourite_languages.py
@@ -15,3 +15,27 @@ favourite_languages = {
 for name, language in favourite_languages.items():
     print(name.title() + "'s favourite language is " +
         language.title() + ".")
+
+
+###print a list of names of people who took this poll
+###.keys() is optional here
+for name in favourite_languages.keys():
+    print(name.title())
+
+###print a message for specific people
+
+friends = ['phil', 'sarah']
+for name in favourite_languages.keys():
+    print(name.title())
+
+    if name in friends:
+        print(" Hi " + name.title() +
+        ", I see your favourite language is " +
+        favourite_languages[name].title() + "!")
+
+
+###use of keys to see if a specific person has been added to the dictionary
+###in this case have they taken the poll as well??
+
+if 'erin' not in favourite_languages.keys():
+    print('Erin, please take the poll!')
