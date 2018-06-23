bfb293183b5b403d737e5ecc1ec3332541b7699f
scass91 <scass1991@gmail.com>
2018-06-23 11:13:06 +0100

    initial commit

---
+++ user.py
@@ -0,0 +1,13 @@
+###June 23rd 2018
+###loop through key-value pairs
+
+
+user_0 = {
+    'username': 'smarsh',
+    'first': 'stan',
+    'last': 'marsh',
+    }
+
+for k, v in user_0.items():
+    print("\nKey: " + k)
+    print("Value: " + v)
