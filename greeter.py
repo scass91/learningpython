0700b464dc1fbd2b2f453acef911578622e8019c
scass91 <scass1991@gmail.com>
2018-06-27 17:41:06 +0100

    added multi-line string

--- greeter.py
+++ greeter.py
@@ -1,3 +1,8 @@
 ###June 23rd 2018
-name = input("Please enter your name: ")
-print("Hello " + name + "!")
+
+###multi-line string, the += operator adds new string to end of prior string
+prompt = "If you tell us who you are, we can personalise what you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+print("\nHello, " + name + "!")
