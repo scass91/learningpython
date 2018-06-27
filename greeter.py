5083de82d53833cd8690988b8b8e7aa69a50c323
scass91 <scass1991@gmail.com>
2018-06-27 17:37:50 +0100

    initial commit

---
+++ greeter.py
@@ -0,0 +1,3 @@
+###June 23rd 2018
+name = input("Please enter your name: ")
+print("Hello " + name + "!")
---
+++ parrot.py
@@ -0,0 +1,3 @@
+###June 27th 2018
+message = input("Tell me something, I will repeat it back to you: ")
+print(message)
