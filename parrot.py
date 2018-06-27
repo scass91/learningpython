249862b2daa5a459d1006957075ffa53c8bcd0ad
scass91 <scass1991@gmail.com>
2018-06-27 18:31:47 +0100

    let user decide when to quit

--- parrot.py
+++ parrot.py
@@ -1,3 +1,7 @@
 ###June 27th 2018
-message = input("Tell me something, I will repeat it back to you: ")
-print(message)
+prompt = "\nTell me something, I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end this program. "
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    print(message)
