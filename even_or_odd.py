c39a2c07f3ef3d67b410911807afb862aafc41af
scass91 <scass1991@gmail.com>
2018-06-27 18:07:02 +0100

    initial commit

---
+++ even_or_odd.py
@@ -0,0 +1,9 @@
+###June 27th 2018
+
+number = input("Enter a number, I will tell you if it's even or odd: ")
+number = int(number)
+
+if number % 2 == 0:
+    print("\nThe number " + str(number) + " is even.")
+else:
+    print("\nThe number " + str(number) + " is odd.")
---
+++ rollercoaster.py
@@ -0,0 +1,9 @@
+###June 27th 2018
+
+height = input("How tall are you, in inches? ")
+height = int(height)
+
+if height >= 36:
+    print("\nYou're tall enough for this rollercoaster!")
+else:
+    print("\nYou'll be able to ride when you're a little older.")
