113a34fbcb4c217859ba1b331010cd6ca65c189a
scass91 <scass1991@gmail.com>
2018-07-06 20:57:37 +0100

    added Pastrami, removed from list

--- deli.py
+++ deli.py
@@ -3,10 +3,17 @@
 ###various sandwiches. Then make an empty list called finished_sandwiches.
 ###Loop through this list and print a message for each order. As each sandwich
 ###is made, move it to the list of finished sandwiches. After all
-###sandwiches have been made, list them all"
+###sandwiches have been made, list them all
+###Make pastrami appear several times in the order list, print a message
+###revealing that the deli has run out of pastrami"
 
-sandwich_orders = ["Tuna sandwich", "Meatball sandwich", "Cheese sandwich"]
+sandwich_orders = ["Tuna", "Meatball", "Cheese", "Pastrami", "Chicken",
+                    "Pastrami", "Ham"]
 finished_sandwiches = []
+
+while "Pastrami" in sandwich_orders:
+    print("Sorry, we have run out of Pastrami!")
+    sandwich_orders.remove("Pastrami")
 
 while sandwich_orders:
     sandwich = sandwich_orders.pop()
