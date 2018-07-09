94d2180dce460b1c653eb53c454a76f04553a8c3
scass91 <scass1991@gmail.com>
2018-07-09 19:32:45 +0100

    initial commit

---
+++ greeting.py
@@ -0,0 +1,14 @@
+###July 9th 2018
+###First delve into functions
+
+###def = defining a function
+def greet_user():
+    ###docstring, i.e. describes the function within triple quotes
+    ###python looks for this when generating documentation for functions
+    ###in programs
+    """Display a simple greeting"""
+    ###the function
+    print("Hello!")
+
+###call the function, any info needed can be put in brackets
+greet_user()
