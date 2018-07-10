80ef8f5b212099b452750a569f8ccdca030e9830
scass91 <scass1991@gmail.com>
2018-07-10 19:33:22 +0100

    initial commit

---
+++ displaymessage.py
@@ -0,0 +1,9 @@
+###July 10th 2018
+###write a function called display_message()that prints one sentence
+###telling everyone what you are learning about
+
+def display_message():
+    """Informs users what I am learning about"""
+    print("I am learning about functions, which are named blocks of code " +
+          "designed to do one job.")
+display_message()
