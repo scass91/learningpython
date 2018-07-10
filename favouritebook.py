d9376ecf4ecd0f84a9430bfd2cf55149543e5ea5
scass91 <scass1991@gmail.com>
2018-07-10 20:06:55 +0100

    initial commit

---
+++ favouritebook.py
@@ -0,0 +1,9 @@
+###July 10th 2018
+###Write a function called favourite_book() that acceots one parameter, title
+###The function should print a message. Call the function, ensuring a book
+###title is used as an argument in the function call
+
+def favourite_book(title):
+    """Display a message which includes your favourite book"""
+    print("My favourite book is " + title.title() + ".")
+favourite_book("the shadow rising")
