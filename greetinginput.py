fac81b994f7389d03e2d5f661ffb341d497e66bf
scass91 <scass1991@gmail.com>
2018-07-10 19:23:27 +0100

    initial commit

---
+++ greetinginput.py
@@ -0,0 +1,8 @@
+###July 10th 2018
+###modify a function to make it applicable to a given input
+
+def greet_user(username):
+    """Display a simple greeting"""
+    print("Hello, " + username.title() + "!")
+
+greet_user("Simon")
