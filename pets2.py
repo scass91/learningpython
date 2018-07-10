5b052c2043bdc300e1a84a99d8be2749b7be930c
scass91 <scass1991@gmail.com>
2018-07-10 20:46:18 +0100

    added default values

--- pets2.py
+++ pets2.py
@@ -8,14 +8,26 @@
 def describe_pet(animal_type, pet_name):
     """Display information about a pet"""
     print("\nEmily has a " + animal_type + ".")
-    print("\nHer " + animal_type + " is called " + pet_name + ".")
+    print("Her " + animal_type + " is called " + pet_name + ".")
 
 describe_pet("cat", "smudge")
 
 #keyword arguments - name value pairs which are passed to a function
-def describe_animal(animal_type, pet_name):
+def describe_pet(animal_type, pet_name):
     """Display information about a pet"""
     print("\nEmily has a " + animal_type + ".")
     print("Her " + animal_type + "'s name is " + pet_name.title() + ".")
 
-describe_animal(animal_type = "cat", pet_name = "smudge")
+describe_pet(animal_type = "cat", pet_name = "smudge")
+
+
+#default values - if an argument value is provided in the function call by
+#default, you can exclude the dorresponding argument in the function called
+#using default values can simplify function calls & clarify ways functions are
+#used
+def describe_pet(pet_name, animal_type = "dog"):
+    """Display information about a pet"""
+    print("\nKatherine has a " + animal_type + ".")
+    print("Her " + animal_type + " is called " + pet_name.title() + ".")
+
+describe_pet(pet_name = "betty")
