88635d4cab569241634575e3d0d3f53207c453da
Simon Cass <scass1991@gmail.com>
2018-07-17 23:10:22 +0100

    initial commit

---
+++ making_pizzas.py
@@ -0,0 +1,7 @@
+#July 16th 2018
+#import pizza.py module to make 2 pizzas
+
+import pizza
+
+pizza.make_pizza(16, "pepperoni")
+pizza.make_pizza(12, "mushrooms", "extra cheese")
--- pizza.py
+++ pizza.py
@@ -5,12 +5,14 @@
 
 #positional & arbitrary arguments can be used in combination
 
+#July 16th 2018
+#import a function by importing an entire module
+#a module is a file ending in .py containing code I wish to import
+#This hides some code, and enables focusing on higher-level logic
+
 def make_pizza(size, *toppings):
     """Summarise the pizza which is about to be made"""
     print("\nMaking a " + str(size) +
          "-inch pizza with the following toppings: ")
     for topping in toppings:
         print("- " + topping)
-        
-make_pizza(16, "pepperoni")
-make_pizza(11, "mushrooms", "salami", "extra cheese")
