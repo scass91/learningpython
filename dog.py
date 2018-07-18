4a5d245ccb1169ae107b9782bee08a6f8a19c5c3
Simon Cass <scass1991@gmail.com>
2018-07-18 19:35:06 +0100

    second instance added

--- dog.py
+++ dog.py
@@ -19,9 +19,13 @@ class Dog():
         print(self.name.title() + " rolled over!")
 
 my_dog = Dog("toe", 6)
+your_dog = Dog("sponge", 1)
 
 print("My dog's name is " + my_dog.name.title() + ".")
 print("My dog is " + str(my_dog.age) + " years old.")
-
 my_dog.sit()
 my_dog.roll_over()
+print("\nYour dog's name is " + your_dog.name.title() + ".")
+print("Your dog is " + str(your_dog.age) + " years old.")
+your_dog.sit()
+your_dog.roll_over()
