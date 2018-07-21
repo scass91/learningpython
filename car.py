489cf132916186764cf6708a6b6825bcc81afb87
Simon Cass <scass1991@gmail.com>
2018-07-21 12:48:52 +0100

    set default mileage value

--- car.py
+++ car.py
@@ -8,11 +8,17 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
+        self.mileage = 0
 
     def get_descriptive_name(self):
         """Return a neatly formatted descriptive name"""
         long_name = str(self.year) + " " + self.make + " " + self.model
         return long_name.title()
 
+    def read_mileage(self):
+        """Print a statment showing vehicle mileage"""
+        print("This car has " + str(self.mileage) + " miles on it.")
+
 my_new_car = Car("mercedes-benz" ,"a-class", 2018)
 print(my_new_car.get_descriptive_name())
+my_new_car.read_mileage()
