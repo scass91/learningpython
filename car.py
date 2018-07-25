c97876dd981a2f304f6328a8d7d666c0ad99057a
Simon Cass <scass1991@gmail.com>
2018-07-25 18:37:01 +0100

    added Battery & ElecCar class

--- car.py
+++ car.py
@@ -34,3 +34,36 @@ class Car():
             self.mileage += miles
         else:
             print("You can't remove miles from an odometer!")
+
+class Battery():
+    """Simple battery modelling attempt"""
+
+    def __init__(self, battery_size=70):
+        """Initialise battery attributes"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print statement describing battery size"""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the mileage provided by this battery"""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
+class ElectricCar(Car):
+    """Models aspects of a car, specific to EVs"""
+
+    def __init__(self, make, model, year):
+        """
+        Initialise attributes of the parent class
+        Then initialse attributes specific to EVs
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
